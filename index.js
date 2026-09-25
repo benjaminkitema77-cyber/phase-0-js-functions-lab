d0b2a763c7c1df26502a7a312c1bc07a9eb44c4c
benjamin <benjaminkitema77@gmail.com>
2026-09-25 23:20:28 +0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+ // 1. Calculate Tax
+function calculateTax(amount) {
+  return amount * 0.16;
+}
+
+// 2. Convert String to Uppercase
+function convertToUpperCase(str) {
+  return str.toUpperCase();
+}
+
+// 3. Find Maximum
+function findMaximum(num1, num2) {
+  return Math.max(num1, num2);
+}
+
+// 4. Check for Palindrome
+function isPalindrome(str) {
+  const lowerCaseStr = str.toLowerCase();
+  const reversedStr = lowerCaseStr.split('').reverse().join('');
+  
+  return lowerCaseStr === reversedStr;
+}
+
+// 5. Calculate Discounted Price
+function calculateDiscountedPrice(price, discount) {
+  return price - (price * discount / 100);
+}
 
 
 
